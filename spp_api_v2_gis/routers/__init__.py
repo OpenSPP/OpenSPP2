41b34e955d640c57a3fd90b57d93e303d2ba5d02
Jeremi Joslin <jeremi@openspp.org>
2026-04-02 16:33:09 +0700

    refactor(spp_api_v2_gis): remove non-OGC GIS endpoints duplicated by OGC API
    The geofence, proximity, and spatial_query routers were fully
    duplicated by OGC API - Features and OGC API - Processes endpoints.
    The QGIS plugin has already migrated to the OGC endpoints.
    
    Removed: /gis/geofences, /gis/query/proximity, /gis/query/statistics
    Kept: /gis/export/geopackage and /gis/statistics (unique, no OGC equivalent)

--- spp_api_v2_gis/routers/__init__.py
+++ spp_api_v2_gis/routers/__init__.py
@@ -1,9 +1,6 @@
 # Part of OpenSPP. See LICENSE file for full copyright and licensing details.
 from . import export
-from . import geofence
 from . import jobs
 from . import ogc_features
 from . import processes
-from . import proximity
-from . import spatial_query
 from . import statistics
--- spp_api_v2_gis/schemas/__init__.py
+++ spp_api_v2_gis/schemas/__init__.py
@@ -1,5 +1,4 @@
 # Part of OpenSPP. See LICENSE file for full copyright and licensing details.
-from . import geofence
 from . import geojson
 from . import ogc
 from . import query
